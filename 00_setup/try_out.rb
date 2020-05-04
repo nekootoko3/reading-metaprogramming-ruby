03f03155eedad02d75c294282277b2e92e9e43c6
nekootoko3 <miyako.kerria@gmail.com>
2020-05-04 22:24:26 +0900

    Refactor

--- 00_setup/try_out.rb
+++ 00_setup/try_out.rb
@@ -5,12 +5,12 @@ class TryOut
   # first_name=メソッドを持つ。これは、引数の内容でファーストネームを書き換える。
   # upcase_full_nameメソッドを持つ。これは、full_nameメソッドの結果をすべて大文字で返す。このメソッドは副作用を持たない。
   # upcase_full_name! メソッドを持つ。これは、upcase_full_nameの副作用を持つバージョンで、ファーストネーム、ミドルネーム、ラストネームをすべて大文字に変え、オブジェクトはその状態を記憶する
-  def initialize(*args)
-    raise ArgumentError unless args.length == 2 || args.length == 3
+  def initialize(*names)
+    raise ArgumentError unless names.length == 2 || names.length == 3
 
-    @first_name = args.first
-    @middle_name = args[1] if args.length == 3
-    @last_name = args.last
+    @first_name = names.first
+    @middle_name = names[1] if names.length == 3
+    @last_name = names.last
   end
 
   def full_name
